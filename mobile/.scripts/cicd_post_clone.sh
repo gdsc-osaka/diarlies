e60a76b465ebc19eedc2aec3a2dde52e0298c063
Harineko0 <harineko0927@gmail.com>
2025-05-03 09:28:51 +0900

    refactor cicd scripts

---
+++ mobile/.scripts/cicd_post_clone.sh
@@ -0,0 +1,3 @@
+(cd mobile && bash ./env.sh)
+(cd mobile && bash ./update_version.sh)
+dart pub global activate flutterfire_cli
--- mobile/.scripts/cicd_pre_build.sh
+++
@@ -1,5 +0,0 @@
-bash ./env.sh
-
-bash ./update_version.sh
-
-dart pub global activate flutterfire_cli
