8fdc1db0ec78ef61033a5d43db8f5217bc1315f7
Harineko0 <harineko0927@gmail.com>
2025-05-03 12:40:23 +0900

    Added hono

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
+
+// import { OpenAPIHono } from '@hono/zod-openapi';
+//
+// const app = new OpenAPIHono();
+//
+// app.post('/', (c) => {
+//   return c.text('Hello Hono!')
+// });
+//
+// export default app;
--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -7,5 +7,9 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['test/**/*.test.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
