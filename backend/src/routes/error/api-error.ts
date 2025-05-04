8afde47e5cba9a773d236d88bdcd0827635a81b8
Harineko0 <harineko0927@gmail.com>
2025-05-04 21:20:55 +0900

    format backend/

--- backend/src/routes/error/api-error.ts
+++ backend/src/routes/error/api-error.ts
@@ -1,17 +1,17 @@
 import z from "zod";
-import {ServiceError} from "../../service/error/service-error";
+import { ServiceError } from "../../service/error/service-error";
 
 export const ApiError = z
-    .object({
-        code: z.string(),
-        message: z.string(),
-    })
-    .openapi({ ref: "ApiError" });
+  .object({
+    code: z.string(),
+    message: z.string(),
+  })
+  .openapi({ ref: "ApiError" });
 export type ApiError = z.infer<typeof ApiError>;
 
 export const convertToApiError = (serviceError: ServiceError): ApiError => {
-    return {
-        code: serviceError.code,
-        message: serviceError.message,
-    };
-}
+  return {
+    code: serviceError.code,
+    message: serviceError.message,
+  };
+};
--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,43 +1,41 @@
-import {Hono} from "hono";
+import { Hono } from "hono";
 import authorize from "./middleware/authorize";
-import {describeRoute} from "hono-openapi";
-import {User} from "../domain/user";
-import {fetchUser} from "../service/user-service";
-import {fetchUserByUid} from "../infra/user-repository";
-import {HTTPException} from "hono/http-exception";
-import {resolver} from "hono-openapi/zod";
-import {ApiError, convertToApiError} from "./error/api-error";
+import { describeRoute } from "hono-openapi";
+import { User } from "../domain/user";
+import { fetchUser } from "../service/user-service";
+import { fetchUserByUid } from "../infra/user-repository";
+import { HTTPException } from "hono/http-exception";
+import { resolver } from "hono-openapi/zod";
+import { convertToApiError } from "./error/api-error";
 
 const app = new Hono();
 const tags = ["Users"];
 
 app.get(
-    "/",
-    authorize,
-    describeRoute({
-        tags,
-        validateResponse: true,
-        description: "Say hello to the user",
-        responses: {
-            200: {
-                description: "Successful response",
-                content: {
-                    "application/json": {
-                        schema: resolver(User),
-                    },
-                },
-            },
-        }}),
-    async (c) => {
-        const res = await fetchUser(fetchUserByUid)(c.get("authUser"));
-        if (res.isErr()) {
-            throw new HTTPException(
-                res.error.status,
-                convertToApiError(res.error),
-            );
-        }
-        return c.json({user: res.value});
+  "/",
+  authorize,
+  describeRoute({
+    tags,
+    validateResponse: true,
+    description: "Say hello to the user",
+    responses: {
+      200: {
+        description: "Successful response",
+        content: {
+          "application/json": {
+            schema: resolver(User),
+          },
+        },
+      },
     },
+  }),
+  async (c) => {
+    const res = await fetchUser(fetchUserByUid)(c.get("authUser"));
+    if (res.isErr()) {
+      throw new HTTPException(res.error.status, convertToApiError(res.error));
+    }
+    return c.json({ user: res.value });
+  },
 );
 
 export default app;
