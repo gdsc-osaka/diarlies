43d389072d63b611135eaace368a3a62476f234d
Harineko0 <harineko0927@gmail.com>
2025-05-12 10:52:46 +0900

    Added Android drawable icons

---
+++ mobile/api/test/create_user_service_error_test.dart
@@ -0,0 +1,36 @@
+import 'package:test/test.dart';
+import 'package:api/api.dart';
+
+// tests for CreateUserServiceError
+void main() {
+  final instance = CreateUserServiceErrorBuilder();
+  // TODO add properties to the builder and call build()
+
+  group(CreateUserServiceError, () {
+    // int status
+    test('to test the property `status`', () async {
+      // TODO
+    });
+
+    // String message
+    test('to test the property `message`', () async {
+      // TODO
+    });
+
+    // String code
+    test('to test the property `code`', () async {
+      // TODO
+    });
+
+    // String brand
+    test('to test the property `brand`', () async {
+      // TODO
+    });
+
+    // User user
+    test('to test the property `user`', () async {
+      // TODO
+    });
+
+  });
+}
